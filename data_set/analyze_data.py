a6c2ccd0dabdf2bcd18049d012c137a2e820cba0
cjyfff <1318434601@qq.com>
2016-02-09 23:39:27 +0800

    [add]add base analyze data

---
+++ data_set/analyze_data.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import pandas as pd
+import load_data_set
+
+
+def analyze():
+    data_mat, label_mat = load_data_set.load('./adult.data')
+    data = pd.DataFrame(data_mat, columns=['age', 'workclass', 'education', 'education-num',
+                                           'marital-status', 'occupation', 'relationship',
+                                           'race', 'sex', 'capital-gain', 'capital-loss',
+                                           'hours-per-week', 'native-country', ])
+    label = pd.Series(label_mat)
+    print('最大年龄： %s' % max(data['age']))
+    print('最小年龄： %s' % min(data['age']))
+    print('正样本数目： %s' % len(label[label == 1]))
+    print('负样本数目： %s' % len(label[label == 0]))
+
+
+if __name__ == '__main__':
+    analyze()
